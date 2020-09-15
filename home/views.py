ada9bbb20445daad810aa49ffe3dba5a7032c804
jojo <nesjoselyne@gmail.com>
2020-09-15 17:57:45 +0200

    updating project

--- home/views.py
+++ home/views.py
@@ -25,27 +25,29 @@ def ussdapp(request):
        level = text.split("*")
        response = ""
        if text == '':
-          response = "CON welcome to ida technology ussd app"
-          response += "1. girls in code\n"
-          response += "2. sdf program\n"
-          response += "3. other\n"
+          response = "CON welcome to ida technology ussd app \n"
+          response += "1. girls in code \n"
+          response += "2. sdf program \n"
+          response += "3. other"
 
-       elif text == '1*1':
-          response = "CON Enter your name"
-       elif text == '1*2':
-          response = "CON Enter your name"
-       elif text == '1*3':
-          response = "CON Enter your name"
        elif text =='1':
           response = "CON welcome to girls in code program"
-          response += "1. join the program"
-          response += "2. get activity"
+          response += "1. join the program\n"
+          response += "2. get activity\n"
           response += "3. leave"
+
+       elif text == '1*1':
+          response = "CON Enter your name \n"
+       elif text == '1*2':
+          response = "CON Enter your name \n"
+       elif text == '1*3':
+          response = "CON Enter your name \n"
+      
        elif text =='2':
-          response = "CON welcome to girls in code program"
-          response += "1. join the program"
-          response += "2. get activity"
-          response += "3. leave"
+          response = "CON welcome to girls in code program \n"
+          response += "1. join the program \n"
+          response += "2. get \n"
+          response += "3. leave \n"
        else:
            response = "END invalid choice "
        return HttpResponse(response)
