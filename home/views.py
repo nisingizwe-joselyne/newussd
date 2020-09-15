1c23b3edbb037c6f44ef3dfa9ac439c111ced39f
jojo <nesjoselyne@gmail.com>
2020-09-15 18:11:07 +0200

    update project

--- home/views.py
+++ home/views.py
@@ -18,37 +18,37 @@ def pricing(request):
 @csrf_exempt
 def ussdapp(request):
     if request.method == 'POST':
-       session_id = request.POST.get("sessionID")
-       service_code = request.POST.get("servicecode")
-       phone_number = request.POST.get("phoneNumber")
-       text = request.POST.get("text")
-       level = text.split("*")
-       response = ""
-       if text == '':
-          response = "CON welcome to ida technology ussd app \n"
-          response += "1. girls in code \n"
-          response += "2. sdf program \n"
-          response += "3. other"
+        # mandatory
+        session_id = request.POST.get('sessionId')
+        service_code = request.POST.get('serviceCode')
+        phone_number = request.POST.get('phoneNumber')
+        text = request.POST.get('text')
+        level = text.split('*')
+        response = ''
 
-       elif text =='1':
-          response = "CON welcome to girls in code program"
-          response += "1. join the program\n"
-          response += "2. get activity\n"
-          response += "3. leave"
+        if text == '':
+            response = 'CON Welcome to Ida USSD app \n'
+            response += '1. Girls In Code \n'
+            response += '2. SDF Program'
+            
+        elif text == '1':
+            response = 'CON Welcome to Girls In Code \n'
+            response += '1. Join the Program \n'
+            response += '2. Get Activity \n'
+            response += '3. Leave'
 
-       elif text == '1*1':
-          response = "CON Enter your name \n"
-       elif text == '1*2':
-          response = "CON Enter your name \n"
-       elif text == '1*3':
-          response = "CON Enter your name \n"
-      
-       elif text =='2':
-          response = "CON welcome to girls in code program \n"
-          response += "1. join the program \n"
-          response += "2. get \n"
-          response += "3. leave \n"
-       else:
-           response = "END invalid choice "
-       return HttpResponse(response)
-    return HttpResponse('welcome')
+        elif text == '1*1':
+            response = 'CON Enter you Name \n'
+        elif text == '1*2':
+            response = 'CON Enter your PINCode \n'
+        elif text == '1*3':
+            response = 'CON Enter your Address \n'
+        else:
+            response = 'END Invalid Choice'
+
+
+            
+
+        return HttpResponse(response)
+
+    return HttpResponse('Welcome')
