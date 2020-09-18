a6a82783cf359ccd3c665ec3404e4a061509f718
jojo <nesjoselyne@gmail.com>
2020-09-18 13:14:55 +0200

    updating new

--- home/views.py
+++ home/views.py
@@ -87,6 +87,12 @@ def digitalapp (request):
             # current harvesting session
         elif text == '1*1':
             response = 'CON Enter farmers code or phone number '+str(len(level))+' \n'
+        
+        elif numb == '1*1' and int(len(level))==3 and str(level[2]) in str(level):
+            response = 'CON you want tocheck on ur : \n'
+            response += '1.total harvesting in  the last 3 months \n'
+            response += '2.total harvesting in  the last 4 months \n'
+            response += '3.total harvesting in  the last 6 months '
         elif text == '1*1*1' :
             response = 'CON your harvesting in the last 3 month is 540kg :\n'
             response += '1.july:220kg \n'
@@ -98,12 +104,6 @@ def digitalapp (request):
             response += '2.july:246kg \n'
             response += '3.august:260kg '
             response += '4.sept:80kg'
-        elif numb == '1*1' and int(len(level))==3 and str(level[2]) in str(level):
-            response = 'CON you want tocheck on ur : \n'
-            response += '1.total harvesting in  the last 3 months \n'
-            response += '2.total harvesting in  the last 4 months \n'
-            response += '3.total harvesting in  the last 6 months '
-        
             # monthly current harvesting
         elif text == '1*2':
             response = 'CON Enter farmers code \n'
