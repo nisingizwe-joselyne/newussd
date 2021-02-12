c054e5bd9b5e3668cc6f4e5ecc55a78130b76492
nisingizwe-joselyne <50444448+nisingizwe-joselyne@users.noreply.github.com>
2021-02-12 10:56:10 -0800

    Update views.py

--- home/views.py
+++ home/views.py
@@ -118,12 +118,13 @@ def digitalapp (request):
             response += '2.currently monthly harvesting'
         elif text == '1*1*1':
             response = 'CON injizamo code yumuhinzi '+str(len(level))+ '\n'
-            y = input
+           
+        elif numb == '1*1*1' and int(len(level))==4 and str(level[3]) in str(level):
+            y = input.save
             y =request.POST['farmercode']
             v=Harvestrecord.objects.filter(farmercode=y)
             for rt in v:
                 qty = rt.Quantity
-        elif numb == '1*1*1' and int(len(level))==4 and str(level[3]) in str(level):
             response = 'CON umusaruro mbumbe wawe ni' + qty + ' \n'
         elif text == '1*1*2':
             response = 'CON Enter  farmers code or phone number \n'
